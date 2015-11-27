4cf0a6e17cc1dabb3df26615b73c7e7758c87927
Florent Bourgeois <florent@creative-area.net>
2015-11-27 11:22:59 +0100

    Allow external MySQL instance connection
    Add the hability to connect to an unlink MySQL database by  passing the MYSQL_SERVER_HOST env variable

--- config.inc.php
+++ config.inc.php
@@ -1,7 +1,9 @@
 <?php
 include 'config.sample.inc.php';
 
-$cfg['Servers'][1]['host'] = 'mysql';
+$server_host = (!empty($_ENV['MYSQL_SERVER_HOST'])) ? $_ENV['MYSQL_SERVER_HOST'] : 'mysql';
+
+$cfg['Servers'][1]['host'] = $server_host;
 $file_with_secret = 'config.inc.secret.php';
 
 if (!file_exists($file_with_secret)) {
